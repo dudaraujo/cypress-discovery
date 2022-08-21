5c132da5a7efb2788f243f09ccf28af7bd162e4f
Duda Araujo <mduda_araujo@hotmail.com>
2022-08-20 21:21:34 -0300

    Organizando Pastas

---
+++ buger-eats/cypress/e2e/home.cy.js
@@ -0,0 +1,8 @@
+
+describe('home page', () => {
+    it('app must be online', () => {
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app'),
+        cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats');
+    })
+})
