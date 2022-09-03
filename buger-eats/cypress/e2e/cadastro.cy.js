f0b7f4c3c333720ef6f492a1158f8540c730ae8a
Duda Araujo <mduda_araujo@hotmail.com>
2022-09-02 23:18:54 -0300

    Inserindo massa de testes em fixtures

--- buger-eats/cypress/e2e/cadastro.cy.js
+++ buger-eats/cypress/e2e/cadastro.cy.js
@@ -6,65 +6,37 @@ import signup from '../pages/SignupPage';
 
 describe('cadastro', () => {
 
+    beforeEach(function() {
+        //Como é um função assincrona, e preciso passar uma promessa (then)
+        //Para pegar o resultado dessa promessa eu uso o this
+        //This pe uma palavra reservada para eu criar um variável de contexto
+        cy.fixture('deliver').then((massa) => {
+
+            //Armazenando todo o conteudo da massa de testes na variável massa
+            //Uso o this para ter acesso a essa informação nos outros casos de teste
+            this.deliver = massa;
+        })
+    })
+
     //Para eu conseguir utilziar as funções que foram instanciadas, 
     //Eu preciso instanciar a class SigupPage 
     //var signup = new SigupPage();
 
-    it('Usuário deve se torar um deliver', () => {
+    it('Usuário deve se torar um deliver', function() {
        
-        //Essa variável é um obejto JS nativo
-        var deliver = {
-            name: 'Maria Eduarda',
-            cpf: '00000014141',
-            email: 'duda@gmail.com',
-            whatsapp: '11999999999',
-            address: {
-                postalcode: '06765000',
-                street: 'Estrada São Francisco',
-                number: '1000',
-                details: 'Ap 45',
-                district: 'Parque Taboão',
-                city_state: 'Taboão da Serra/SP'
-            },
-            delivery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-
-        }
-
         signup.go();
-        signup.fillForm(deliver);
+        signup.fillForm(this.deliver.signup);
         signup.submit();
 
         const expetedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.';
         signup.modalContentShouldBe(expetedMessage);
 
-
     })
 
-    it('CPF incorreto', () => {
-
-        //Essa variável é um obejto JS nativo
-        var deliver = {
-            name: 'Maria Eduarda',
-            cpf: '00000014dd1',
-            email: 'duda@gmail.com',
-            whatsapp: '11999999999',
-            address: {
-                postalcode: '06765000',
-                street: 'Estrada São Francisco',
-                number: '1000',
-                details: 'Ap 45',
-                district: 'Parque Taboão',
-                city_state: 'Taboão da Serra/SP'
-            },
-            delivery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-
-        }
-
+    it('CPF incorreto', function() {
 
         signup.go();
-        signup.fillForm(deliver);
+        signup.fillForm(this.deliver.cpf_inv);
         signup.submit();
         signup.alertMessageShouldBe('Oops! CPF inválido');
 
